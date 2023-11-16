04d82adad1b7132cbd044cc37d5e3e67c51012cf
Joey Maffiola <7maffiolajoey@gmail.com>
2023-11-15 19:21:15 -0600

    Password 2 is still elusive

---
+++ Assignment 4 (Password Cracking)/RainbowTable.java
@@ -0,0 +1,107 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A class representing a Rainbow Table used for password cracking.
+ */
+public class RainbowTable {
+    private Map<String, String> table = new HashMap<>();
+    private int maxLength;
+
+    /**
+     * The table is generated with a maximum length specified by the user.
+     */
+    public RainbowTable(int maxLength) {
+        this.maxLength = maxLength;
+        generateTable(maxLength);
+    }
+
+    /**
+     * This constructor generates a table of possible passwords and can be used to
+     * crack hashed passwords.
+     */
+    public RainbowTable(List<String> possiblePasswords) {
+        generateTable(possiblePasswords);
+    }
+
+    /**
+     * Generates a rainbow table for passwords up to a specified maximum length.
+     * 
+     * @param maxLength the maximum length of passwords to include in the table
+     */
+    private void generateTable(int maxLength) {
+        for (int length = 1; length <= maxLength; length++) {
+            generateTable("", length);
+        }
+    }
+
+    /**
+     * Generates a rainbow table of MD5 hashes for all possible combinations of
+     * characters of a given length.
+     * The characters are limited to digits, uppercase letters, and lowercase
+     * letters (Alphanumeric).
+     * 
+     * @param current   the current combination of characters being generated
+     * @param remaining the number of characters remaining to be added to the
+     *                  current combination
+     */
+    private void generateTable(String current, int remaining) {
+        if (remaining == 0) {
+            table.put(current, md5(current));
+        } else {
+            // ASCII values for digits
+            for (int i = 48; i <= 57; i++) {
+                generateTable(current + (char) i, remaining - 1);
+            }
+            // ASCII values for uppercase letters
+            for (int i = 65; i <= 90; i++) {
+                generateTable(current + (char) i, remaining - 1);
+            }
+            // ASCII values for lowercase letters
+            for (int i = 97; i <= 122; i++) {
+                generateTable(current + (char) i, remaining - 1);
+            }
+        }
+    }
+
+    // Generate a rainbow table of MD5 hashes for every string in an array
+    public void generateTable(List<String> possiblePasswords) {
+        for (String password : possiblePasswords) {
+            table.put(password, md5(password));
+        }
+    }
+
+    /**
+     * Calculates the MD5 hash of the given password.
+     *
+     * @param password the password to hash
+     * @return the MD5 hash of the password
+     */
+    private String md5(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // print table
+    public void print() {
+        for (Map.Entry<String, String> entry : table.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public Map<String, String> getTable() {
+        return table;
+    }
+}
---
+++ Assignment 4 (Password Cracking)/passwordCracker.java
@@ -0,0 +1,179 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class passwordCracker {
+    public static void main(String[] args) {
+        String target1hash = "7b0ca5c95a9398a2f32613d987428180";
+
+        ArrayList<String> possiblePasswords1 = new ArrayList<String>() {
+            {
+                add("19891989");
+                add("1985Bob");
+                add("JaneBob");
+                add("BobOrange");
+                add("CharlieBob");
+                add("Alice1989");
+                add("BobCharlie");
+                add("1985Orange");
+                add("AliceBob");
+                add("Jane1989");
+                add("BobBob");
+                add("19851985");
+                add("BobSeattle");
+                add("1985Jane");
+                add("Jane1985");
+                add("AliceJane");
+                add("1989Jane");
+                add("JaneJane");
+                add("AliceAlice");
+                add("Orange1989");
+                add("1985Alice");
+                add("1989Alice");
+                add("Orange1985");
+                add("Bob1989");
+                add("BobMadison");
+                add("SeattleBob");
+                add("Alice1985");
+                add("19891985");
+                add("OrangeBob");
+                add("JaneAlice");
+                add("BobAlice");
+                add("JaneOrange");
+                add("OrangeJane");
+                add("MadisonBob");
+                add("BobJane");
+                add("19851989");
+                add("1989Bob");
+                add("1989Orange");
+                add("Bob1985");
+                add("Orange85");
+                add("85Orange");
+                add("JaneBob");
+                add("89Seattle");
+                add("BobOrange");
+                add("CharlieBob");
+                add("85Bob");
+                add("Charlie89");
+                add("BobCharlie");
+                add("85Jane");
+                add("85Madison");
+                add("85Alice");
+                add("AliceBob");
+                add("89Charlie");
+                add("89Bob");
+                add("BobBob");
+                add("BobSeattle");
+                add("AliceJane");
+                add("Jane89");
+                add("Bob85");
+                add("Madison85");
+                add("Seattle85");
+                add("JaneJane");
+                add("AliceAlice");
+                add("89Jane");
+                add("Charlie85");
+                add("85Seattle");
+                add("Bob89");
+                add("BobMadison");
+                add("SeattleBob");
+                add("Alice85");
+                add("89Alice");
+                add("OrangeBob");
+                add("85Charlie");
+                add("JaneAlice");
+                add("BobAlice");
+                add("Madison89");
+                add("Seattle89");
+                add("Jane85");
+                add("JaneOrange");
+                add("OrangeJane");
+                add("89Madison");
+                add("CharlieJane");
+                add("MadisonBob");
+            }
+        };
+
+        String password1 = new passwordCracker().crackPassword(target1hash, possiblePasswords1);
+
+        System.out.println("The password is: " + password1);
+
+        String target2hash = "7e985df169d043112b23508a81e16538";
+
+        ArrayList<String> personalData = new ArrayList<String>() {
+            {
+                add("Name: Dr. Alice Pearson");
+                add("Date of Birth: November 11, 1983");
+                add("Born: Fort Atkinson, WI");
+                add("Residence: Whitewater, WI");
+                add("Employer: University of Wisconsin- Whitewater");
+                add("Occupation: Cybersecurity Instructor & Volleyball Coach");
+                add("Favorite Color: Purple");
+                add("Publicly-known information from the university newsletter: Travels frequently for cybersecurity conferences and volleyball tournaments");
+                add("Very involved with the university recruitment committee");
+                add("Publicly-visible social media feed includes several examples of: Recreational fishing in Lake Michigan and the Mississippi River");
+                add("Reunion events with former students");
+                add("Anything related to The Beatles");
+            }
+        };
+        List<String> possiblePasswords2 = generatePossiblePasswords(personalData); // Generate possible
+        String password2 = new passwordCracker().crackPassword(target2hash, possiblePasswords2);
+
+        System.out.println("The password is: " + password2);
+
+    }
+
+    public String crackPassword(String targetHash) { // Brute force method
+        RainbowTable[] tables = new RainbowTable[6]; // Array of rainbow tables
+
+        for (int i = 0; i < tables.length; i++) { // Initialize rainbow tables
+            tables[i] = new RainbowTable(i + 5);
+        }
+
+        for (int i = 0; i < tables.length; i++) { // Iterate through rainbow tables
+            for (String key : tables[i].getTable().keySet()) { // Iterate through keys in rainbow table
+                if (tables[i].getTable().get(key).equals(targetHash)) { // Check if hash matches target hash
+                    return key; // Return password
+                }
+            }
+        }
+        return "Password not found.";
+    }
+
+    public String crackPassword(String targetHash, List<String> possiblePasswords) { // Rainbow table method
+        RainbowTable table = new RainbowTable(possiblePasswords); // Initialize rainbow table
+
+        for (String key : table.getTable().keySet()) { // Iterate through keys in rainbow table
+            if (table.getTable().get(key).equals(targetHash)) { // Check if hash matches target hash
+                return key; // Return password
+            }
+        }
+        return "Password not found. Here are the passwords that were tried:\n" + possiblePasswords.toString() + "\n";
+    }
+
+    public static List<String> generatePossiblePasswords(List<String> personalData) {
+        List<String> possiblePasswords = new ArrayList<>();
+
+        for (String data : personalData) {
+            // Check if the data length is within the old password constraints
+            if (data.length() >= 5 && data.length() <= 10 && data.matches("[a-zA-Z0-9]+")) {
+                // Generate extensions of the old password to meet the new constraints
+                for (int i = 12; i <= 20; i++) {
+                    StringBuilder extendedPassword = new StringBuilder(data);
+                    while (extendedPassword.length() < i) {
+                        // Append numbers and letters to meet the new length requirement
+                        // This is a simple example, you can use more complex logic
+                        extendedPassword.append("1"); // Example of appending a number
+                        if (extendedPassword.length() < i) {
+                            extendedPassword.append("a"); // Example of appending a letter
+                        }
+                    }
+                    possiblePasswords.add(extendedPassword.toString());
+                }
+            }
+        }
+
+        return possiblePasswords;
+    }
+
+}
