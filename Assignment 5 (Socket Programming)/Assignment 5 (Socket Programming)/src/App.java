622fd0336715a2dd0a128bdd9a0ea716f39a5d4e
Joey Maffiola <7maffiolajoey@gmail.com>
2023-11-18 17:32:46 -0600

    Client draft complete! Testing needed.

--- Assignment 5 (Socket Programming)/Assignment 5 (Socket Programming)/src/App.java
+++ Assignment 5 (Socket Programming)/Assignment 5 (Socket Programming)/src/App.java
@@ -1,20 +1,32 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-import javax.xml.bind.PrintConversionEvent;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        // Create a socket
-        Socket socket = new Socket("127.0.0.1", 51234);
-        // Create print writer to send data over network
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        // Create a buffered reader to recive data over network
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        Socket socket = null;
+        try {
+            // Create a socket
+            socket = new Socket("127.0.0.1", 51234);
+            // Create print writer to send data over network
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            // Create a buffered reader to recive data over network
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        // Send a request
-        writer.println("My first socket request!");
+            // Send a request
+            String request = "My first socket request!";
+            writer.println(request);
+            // Listen for response
+            String response = reader.readLine();
+            System.out.println("Response: " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the socket
+            socket.close();
+        }
+
     }
 }
